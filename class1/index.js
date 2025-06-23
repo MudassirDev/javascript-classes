5a9c4112ea183427038e834712bd884d313e7245
mudassir <mughalmudassir966@gmail.com>
2025-06-23 22:23:42 +0500

    lesson1 complete

---
+++ class1/index.js
@@ -0,0 +1,7 @@
+// variable sirf naam hote hain jo identify krte hain kisi stored value ko
+// let or var mein difference ye hota hai ke var ko aap dubara redeclare krsakte ho, let ko aap dubara redeclare nahi kr sakte
+var person1 = "Ahmed";
+let person2 = "Mudassir";
+const person3 = "Abbass";
+
+console.log(person1, person2, person3)
