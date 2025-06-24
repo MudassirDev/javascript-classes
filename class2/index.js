0ef0be4325902cf7e7dd838da023821aeeee7fc4
Mudassir Bilal <mughalmudassir966@gmail.com>
2025-06-24 22:40:26 +0500

    class 2

---
+++ class2/index.js
@@ -0,0 +1,32 @@
+// javascript ke andar har line of code aik statement hai
+// statements multiline aur single dono hosakti hai
+const person1 = "Mudassir";
+const person9 = `
+asdasd
+asdas`; // ye multiline statement
+
+// expression wo code hota hai jo execute hoke kuch value ya outcome deraha hota hai
+const a = 1;
+const b = 5;
+const c = b / a;
+console.log(c);
+
+// do tarah ke data types hote hain, primitive aur non primitive data types
+const asdassadas = "ye kuch bhi 1 hai"; // string
+console.log(asdassadas, typeof asdassadas);
+
+const njkkajn = 1; // number
+console.log(njkkajn, typeof njkkajn);
+
+const asdasd = false; // boolean
+console.log(asdasd, typeof asdasd);
+
+let person3; // undefined
+console.log(person3, typeof person3);
+
+let person4 = null; // object
+console.log(person4, typeof person4);
+
+// variable naming rule | camel case
+const yeAikDabbaHai = "ok";
+const DATABASE_KEY = "";
