b0813b3cc76e767ee564dba18cc1fcaa7c7e92e5
Mudassir Bilal <mughalmudassir966@gmail.com>
2025-06-26 23:24:39 +0500

    ok

---
+++ class4/index.js
@@ -0,0 +1,47 @@
+// statements
+// code ke ander har aik line aik statement hoti hai, statement aik line ki bhi hosakti hai aur multiple lines ki bhi hosakti hai
+
+// variables
+// variable aik box hai jiske andar hum kuch place kr rahe hote hain
+// variable declare krne ke teen tarike hote hain
+
+// first way
+var person1 = "ali";
+
+// second way
+let person2 = "ali";
+person2 = "soban";
+
+// third way
+const person3 = "ali";
+
+// mathematics operators
+const value1 = 5;
+const value2 = 10;
+const value3 = value1 * value2;
+console.log(value3);
+
+
+// data types
+
+// two types of data
+// primitive
+
+// string, koi bhi text humara string hota hai
+const car1 = "toyota";
+
+// number, real number like 1, 2, 4, 5
+const carCost = 123;
+
+// boolean, true or false
+const isCar = true;
+
+// undefined, undefined value nahi hai, absence of value
+let ok;
+console.log(ok);
+
+// null, null defines emptiness of value
+const car = null;
+console.log(null);
+
+// non primitive
