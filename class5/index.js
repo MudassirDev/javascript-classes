98c130477e8bc3802631837d76958b95e257f682
Mudassir Bilal <mughalmudassir966@gmail.com>
2025-06-27 22:40:32 +0500

    ok

---
+++ class5/index.js
@@ -0,0 +1,74 @@
+// functions
+// function aik block of code hota hai jo hum kisi bhi time pr run krsakte
+
+// day 1
+// console.log("wake up");
+// console.log("freshen up");
+// console.log("have breakfast");
+// console.log("go to work/school/college/uni");
+// console.log("tution")
+
+// day 2
+// console.log("wake up");
+// console.log("freshen up");
+// console.log("have breakfast");
+// console.log("go to work/school/college/uni");
+// console.log("tution")
+
+// day 3
+// console.log("wake up");
+// console.log("freshen up");
+// console.log("have breakfast");
+// console.log("go to work/school/college/uni");
+// console.log("tution")
+
+// day 4
+// console.log("wake up");
+// console.log("freshen up");
+// console.log("have breakfast");
+// console.log("go to work/school/college/uni");
+// console.log("tution")
+
+// day 5
+// console.log("wake up");
+// console.log("freshen up");
+// console.log("have breakfast");
+// console.log("go to work/school/college/uni");
+// console.log("tution")
+
+// day 6
+// console.log("wake up");
+// console.log("freshen up");
+// console.log("have breakfast");
+// console.log("go to work/school/college/uni");
+// console.log("tution")
+
+// function morningRoutine() {
+//     console.log("wake up");
+//     console.log("freshen up");
+//     console.log("have breakfast");
+//     console.log("go to work/school/college/uni");
+// }
+
+// morningRoutine();
+// morningRoutine();
+// morningRoutine();
+// morningRoutine();
+// morningRoutine();
+// morningRoutine();
+
+function morningRoutine(day, name, age) {
+    console.log("day", day);
+    console.log("this routine is of", name, "who is", age, "years old");
+    console.log("wake up");
+    console.log("freshen up");
+    console.log("have breakfast");
+    console.log("go to work/school/college/uni");
+}
+
+morningRoutine(1, "mudassir", "10");
+morningRoutine(1, "abbas", "22");
+morningRoutine(1, "umair", "28");
+morningRoutine(1, "ahmed", "20");
+morningRoutine(1, "ali", "1");
+morningRoutine(1, "muzakkar", "6");
