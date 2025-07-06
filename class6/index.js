03920d9f9fa377206515fd7ba151032b683493a0
Mudassir <mughalmudassir966@gmail.com>
2025-07-06 21:45:50 +0500

    ok

---
+++ class6/index.js
@@ -0,0 +1,34 @@
+// wo code jo repeat ho use loop kehte hain
+// for loop
+for (let i = 10; i > 0; i--) {
+    // code yahan likhte hain
+    console.log("ok", i)
+}
+
+// while
+let i = 0;
+while (i < 10) {
+    console.log("this is from while loop")
+    i++;
+}
+
+// do while
+do {
+    console.log("this is from do while")
+} while (false);
+
+
+// for off
+// we will study this after arrays
+
+
+// if/else, conditional statement
+const person = "ali";
+
+if (person == "abbass") {
+    console.log("abbass is here");
+} else if (person == "ali") {
+    console.log("ali is here");
+} else {
+    console.log("mudassir is here");
+}
